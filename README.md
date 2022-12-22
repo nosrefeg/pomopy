0fe767804d7c7fb95ec7bf1b57ab107c3178da79
Geferson Araujo <gefersonaraujo00@outlook.com>
2022-12-21 22:15:40 -0300

    Nova versão do código do POMOPYGUI e atualizações do README

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # POMOPY :tomato::hourglass_flowing_sand:
-## Gerenciador de tempo feito em Python, usando a técnica POMODORO, muito difundida e utilizada!
+## Gerenciador de tempo feito em Python com interface gráfica construída através do PySimpleGUI, que usa a técnica POMODORO, muito difundida e utilizada para estudos e foco!
 ## 
--- pomopyGUI.py
+++ pomopyGUI.py
@@ -1,10 +1,26 @@
 import PySimpleGUI as sg
 from time import sleep
-# import threading
+from threading import Thread
+
+sg.theme('DarkPurple')
 
 
+def tempoDeDescanso(window, descanso = 5):
+    """
+    Função que determina o tempo de descanso entre cada pomodoro(loop)
+    Lê os dados da janela e faz loop para representar o tempo de descanso entre cada pomodoro
+    :param window: Janela da aplicação
+    :param descanso: (int) Tempo em minutos que define o loop, ou seja, tempo de descanso do usuário entre cada pomodoro(loop)
+    :return:
+    """
+    for i in range(descanso, 0, -1):
+        window['-TEMPO-'].update(f'{i}')
+        window['-TEXTO-DESCANSO-'].update('MINUTOS DE DESCANSO')
+        window.refresh()
+        sleep(60)
 
-def tempoDeEspera(window, minutos):
+
+def tempoDeEspera(window, minutos, pomodoros):
     """
     Função que determina o tempo de cada pomodoro(loop)
     Lê os dados da janela e os minutos que foram escolhidos pelo usuário
@@ -12,61 +28,62 @@ def tempoDeEspera(window, minutos):
     :param minutos: (int) Tempo em minutos que define o loop, ou seja, a duração de cada pomodoro(loop)
     :return:
     """
-    for i in range(minutos, 0, -1):
-        window['-TEXTO-'].update(f'{i}')
-        window.refresh()
-        sleep(60)
-
-
-def tempoDeDescanso(window, descanso = 5):
-    """
-    Função que determina o tempo de descanso entre cada pomodoro(loop)
-    Lê os dados da janela e 
-    :param window: Janela da aplicação
-    :param descanso: (int) Tempo em minutos que define o loop, ou seja, tempo de descanso do usuário entre cada pomodoro(loop)
-    :return:
-    """
-    for i in range(descanso, 0, -1):
-        if descanso == 10:
-            window['-TEXTO-'].update(f'Você completou um ciclo de 4 pomodoros, descanse um pouco mais!')
+    for i in range(0, pomodoros):
+        for i in range(minutos, 0, -1):
+            window['-TEMPO-'].update(f'{i}')
             window.refresh()
-        window['-TEXTO-'].update(f'{i} minutos de descanso')
-        window.refresh()
-        sleep(60)
+            sleep(60)
+        tempoDeDescanso(window)
+        window['-TEXTO-DESCANSO-'].update('')
+    window.close()
 
 
-
-def pomopy():
-    sg.theme('DarkPurple')
-
+def tela1():
     layout = [
         [sg.Push(), sg.Text('Bem Vindo ao POMOPY, seu gerenciador de tempo feito em Python!', font=('Orbitron', 14, 'bold')), sg.Push()],
         [sg.HSep()],
         [sg.Push(), sg.Text('Escolha quantos Pomodoros deseja:', font=('Orbitron', 14)), sg.Push()],
-        [sg.Push(), sg.Slider(range=(1, 20), default_value=4, key='-SLIDER-', orientation='h', size=(50, 20)), sg.Push()],
+        [sg.Push(), sg.Slider(range=(1, 20), default_value=4, key='-SLIDER-', orientation='h', size=(50, 20), enable_events=True), sg.Push()],
         [sg.HSep()],
         [sg.Push(), sg.Text('Escolha quantos minutos para cada Pomodoro:', font=('Orbitron', 14)), sg.Push()],
         [sg.Push(), sg.Push(), sg.Button('20', key='-20-'), sg.Push(), sg.Button('25', key='-25-'), sg.Push(), sg.Button('30', key='-30-'), sg.Push(), sg.Push()],
         [sg.HSep()],
-        [sg.Push(), sg.Button('INICIAR', key='-INICIAR-', size=(10, 2)), sg.Button('CANCELAR', key='-CANCELAR-', size=(10, 2)), sg.Push()],
+        [sg.Push(), sg.Button('INICIAR', key='-INICIAR-', size=(10, 2)), sg.Button('ZERAR VALORES', key='-ZERAR-', size=(10, 2)), sg.Button('SAIR', key='-SAIR-', size=(10, 2)), sg.Push()],
         [sg.Push(), sg.Text('POMODOROS:', font=('Orbitron', 14)),sg.Text('', key='-POMODOROS-'),sg.VSep(), 
         sg.Text('MINUTOS:', font=('Orbitron', 14)),sg.Text('', key='-MINUTOS-'), sg.Push()],
-        [sg.Push(), sg.Text('', enable_events=True, key='-TEXTO-', font=('Orbitron', 40, 'bold')), sg.Push()],
     ]
+    return sg.Window('POMOPY', layout, location=(800,400),font=('Orbitron', 12),
+                    titlebar_background_color=sg.theme_background_color(),
+                    titlebar_text_color=sg.theme_text_color(),
+                    titlebar_font='Orbitron', finalize=True)
 
 
-    window = sg.Window(
-        'POMOPY', size=(800, 500), layout=layout, font=('Orbitron', 12), use_custom_titlebar=True,
-        titlebar_background_color=sg.theme_background_color(), titlebar_text_color=sg.theme_text_color(),
-        titlebar_font='Orbitron', titlebar_icon='icone_pomopy.png'
-    )
-    minutos = 0
+def tela2():
+    layout = [
+        [sg.Push(), sg.Text('', key='-TEMPO-', font=('Orbitron', 40, 'bold')), sg.Push()],
+        [sg.Push(), sg.Text('', key='-TEXTO-DESCANSO-', font=('Orbitron', 14, 'bold')), sg.Push()],
+        [sg.HSep()],
+        [sg.Push(), sg.Button('CANCELAR', key='-CANCELAR-', size=(10, 2)), sg.Push()]
+    ]
+    return sg.Window('POMOPY', layout, size=(300, 200),font=('Orbitron', 12),
+                    titlebar_background_color=sg.theme_background_color(),
+                    titlebar_text_color=sg.theme_text_color(),
+                    titlebar_font='Orbitron', finalize=True)
 
+
+def main():
+    window1, window2 = tela1(), None
+    
+    minutos = pomodoros = 0
+    
     while True:
-        event, values = window.read()
-
-        pomodoros = int(values['-SLIDER-'])
-
+        window, event, values = sg.read_all_windows()
+        
+        print(event, values)
+        if event == '-SLIDER-':
+            window['-POMODOROS-'].update(int(values['-SLIDER-']))
+            pomodoros = int(values['-SLIDER-'])
+            
         match(event):
             case '-20-':
                 minutos = 20
@@ -74,33 +91,31 @@ def pomopy():
                 minutos = 25
             case '-30-':
                 minutos = 30
-
-        window['-POMODOROS-'].update(pomodoros)
-        window['-MINUTOS-'].update(minutos)
-        window.refresh()
-
-        if event in (sg.WINDOW_CLOSED, sg.WIN_CLOSE_ATTEMPTED_EVENT):
-            break 
-
-        if minutos == 0:
-            continue
+        window1['-MINUTOS-'].update(minutos)
         
-        if event == '-CANCELAR-':
-            minutos = 0
-            pomodoros = 0
-            window['-TEXTO-'].update('')
-            window.refresh()
+        if event in (sg.WINDOW_CLOSED, sg.WIN_CLOSE_ATTEMPTED_EVENT, '-SAIR-', '-CANCELAR-'):
+            window.close()
+            
+            if window == window2:
+                window2 = None
+            elif window == window1:
+                break
+        elif event == '-INICIAR-':
+            if pomodoros == 0 or minutos == 0:
+                sg.popup('Verifique os valores!', 'Defina os valores para o programa!')
+                continue
 
-        if event == '-INICIAR-':
-            for pomodoros in range(0, pomodoros):
-                # threading.Thread(target=tempoDeEspera, args=(window, minutos), daemon=True).start()
-                tempoDeEspera(window, minutos)
-                tempoDeDescanso(window)
-            if pomodoros % 4 == 0 and pomodoros != 0:
-                tempoDeDescanso(window, 10)
-            window['-TEXTO-'].update('Parabéns, você concluiu sua sessão!', font=('Orbitron', 30))
-            window.refresh()
-    window.close()
+            window['-POMODOROS-'].update(pomodoros)
+            window['-MINUTOS-'].update(minutos)
+            window2 = tela2()
+            
+            Thread(target=tempoDeEspera, args=(window2, minutos, pomodoros), daemon=True).start()
+
+        elif event == '-ZERAR-':
+            minutos = pomodoros = 0
+            window['-POMODOROS-'].update(pomodoros)
+            window['-MINUTOS-'].update(minutos)
+
 
 if __name__ == '__main__':
-    pomopy()
+    main()
